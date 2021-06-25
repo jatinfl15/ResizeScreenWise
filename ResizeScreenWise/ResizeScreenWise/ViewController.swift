c532823114a4364a63af20f3f381264a1e3a2828
jatinfl15 <70931173+jatinfl15@users.noreply.github.com>
2021-06-25 14:31:39 +0530

    Add files via upload

---
+++ ResizeScreenWise/ResizeScreenWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResizeScreenWise
+//
+//  Created by Martis on 25/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewResize: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
